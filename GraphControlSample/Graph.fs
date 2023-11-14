6455671f1596cdf50e3f2d69b75c1b2477e58faa
Faisal Waris <Faisal.Waris1@T-Mobile.com>
2023-11-14 11:41:26 -0500

    Add project files.

---
+++ GraphControlSample/Graph.fs
@@ -0,0 +1,44 @@
+﻿namespace GraphSample
+
+open Avalonia.FuncUI.DSL
+
+[<AutoOpen>]
+module GraphPanel =
+    open Avalonia.FuncUI.Types
+    open Avalonia.FuncUI.Builder
+    open AvaloniaGraphControl
+    open Avalonia.Controls.Templates
+    
+    let create (attrs: IAttr<GraphPanel> list): IView<GraphPanel> =
+        ViewBuilder.Create<GraphPanel>(attrs)
+    
+    type GraphPanel with
+        static member graph<'t when 't :> GraphPanel>(value:Graph) : IAttr<'t> = 
+          AttrBuilder<'t>.CreateProperty<Graph>(GraphPanel.GraphProperty, value, ValueNone)          
+
+        static member itemTemplate<'t when 't :> GraphPanel>(value: IDataTemplate) : IAttr<'t> =
+          let getter : ('t -> IDataTemplate) = (fun t -> if t.DataTemplates.Count > 0 then  t.DataTemplates.[0] else null)
+          let setter : ('t * IDataTemplate -> unit) = (fun (t,v) -> t.DataTemplates.Clear(); t.DataTemplates.Add(v))
+          AttrBuilder<'t>.CreateProperty<IDataTemplate>("itemTemplate", value, ValueSome getter, ValueSome setter, ValueNone)
+
+        static member dataTemplates<'t when 't :> GraphPanel>(value: DataTemplates) : IAttr<'t> =
+          let getter : ('t -> DataTemplates) = (fun t -> if t.DataTemplates = null then new DataTemplates() else t.DataTemplates)
+          let setter : ('t * DataTemplates -> unit) = (fun (t,v) -> t.DataTemplates.Clear(); t.DataTemplates.AddRange(v))
+          AttrBuilder<'t>.CreateProperty<DataTemplates>("DataTemplates", value, ValueSome getter, ValueSome setter, ValueNone)
+
+[<AutoOpen>]
+module TextSticker =
+    open Avalonia.FuncUI.Types
+    open Avalonia.FuncUI.Builder
+    open AvaloniaGraphControl
+  
+    let create (attrs: IAttr<TextSticker> list): IView<TextSticker> =
+        ViewBuilder.Create<TextSticker>(attrs)
+    
+    type TextSticker with
+        static member shape<'t when 't :> TextSticker>(value:TextSticker.Shapes) : IAttr<'t> = 
+          AttrBuilder<'t>.CreateProperty<TextSticker.Shapes>(TextSticker.ShapeProperty, value, ValueNone)          
+
+        static member text<'t when 't :> TextSticker>(value:string) : IAttr<'t> = 
+          AttrBuilder<'t>.CreateProperty<string>(TextSticker.TextProperty, value, ValueNone)          
+
---
+++ GraphControlSample/Program.fs
@@ -0,0 +1,114 @@
+﻿namespace GraphSample
+open System
+open Elmish
+open Avalonia.Controls
+open Avalonia.Media
+open Avalonia.FuncUI
+open Avalonia.FuncUI.DSL
+open Avalonia.FuncUI.Hosts
+open Avalonia.FuncUI.Elmish
+open AvaloniaGraphControl
+open Avalonia
+open Avalonia.Controls.ApplicationLifetimes
+open Avalonia.Themes.Fluent
+
+module GraphApp =
+    type [<CLIMutable>] Node = {Label:string}
+    let order (a,b) = if a >= b then a,b else b,a
+
+    let deviceRels =
+      [
+        "gnodeb_id", ["cell_id"; "rmod"; "bbmod"]
+        "enodeb_id", ["cell_id"; "rmod"; "bbomd"]
+        "cell_id", ["sector_id"]
+        "rmod",["sfp"]
+        "bbmod",["sfp"; "smod"; "cabinet"]
+        "smod", ["cabinet"]
+      ]
+
+    let sampleGraph = new AvaloniaGraphControl.Graph()
+    sampleGraph.Edges.Clear()
+    deviceRels |> List.collect (fun (r,xs) -> xs |> List.map(fun b -> order(r,b))) |> List.distinct
+    |> List.iter (fun (a,b) -> 
+      sampleGraph.Edges.Add(AvaloniaGraphControl.Edge(
+        {Label=a},
+        {Label=b},
+        tailSymbol = AvaloniaGraphControl.Edge.Symbol.None,
+        headSymbol = AvaloniaGraphControl.Edge.Symbol.None)))
+
+    type State = { count : int ; graph:AvaloniaGraphControl.Graph}
+    let init = { count = 0; graph=sampleGraph}
+
+    type Msg = Increment | Decrement | Reset
+
+    let update (msg: Msg) (state: State) : State =
+        match msg with
+        | Increment -> { state with count = state.count + 1 }
+        | Decrement -> { state with count = state.count - 1 }
+        | Reset -> init
+    
+    let view (state: State) (dispatch) =
+      Viewbox.create [
+        Viewbox.child (
+          GraphPanel.create [
+            GraphPanel.graph state.graph
+            GraphPanel.itemTemplate (
+                  DataTemplateView<Node>.create (fun (data:Node) ->
+                    TextSticker.create [
+                      TextSticker.shape TextSticker.Shapes.Ellipse
+                      TextSticker.text data.Label
+                    ])                        
+            )
+
+            // GraphPanel.dataTemplates (
+            //   let ds = DataTemplates()
+            //   ds.AddRange(
+            //     [
+            //       DataTemplateView<Node>.create (fun (data:Node) ->
+            //         TextSticker.create [
+            //           TextSticker.shape TextSticker.Shapes.Ellipse
+            //           TextSticker.text data.Label
+            //         ])          
+            //     ])
+            //   ds
+            //   )
+
+            ]
+            )            
+          ]
+
+type MainWindow() as this =
+    inherit HostWindow()
+    do
+        base.Title <- "BasicTemplate"
+        base.Background <- Brushes.White
+        base.Width <- 400.0
+        base.Height <- 400.0
+        Program.mkSimple (fun () -> GraphApp.init) GraphApp.update GraphApp.view
+        |> Program.withHost this
+        |> Program.run
+
+type App() =
+    inherit Application()
+
+    override this.Initialize() =
+        this.Styles.Add (FluentTheme())
+        this.RequestedThemeVariant <- Styling.ThemeVariant.Dark
+
+    override this.OnFrameworkInitializationCompleted() =
+        match this.ApplicationLifetime with
+        | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime ->
+            let mainWindow = MainWindow()
+            desktopLifetime.MainWindow <- mainWindow
+        | _ -> ()
+
+module Program =
+
+    [<EntryPoint>]
+    let main(args: string[]) =
+        AppBuilder
+            .Configure<App>()
+            .UsePlatformDetect()
+            .UseSkia()
+            .StartWithClassicDesktopLifetime(args)
+            
